d1a132631a14bea89d8d34c51221b6b4c433f89f
Jonathan van der Sluijs <74247370+JonathanSluijs@users.noreply.github.com>
2026-09-15 02:36:16 +0200

    Update config.js

--- sales-pwa/config.js
+++ sales-pwa/config.js
@@ -3,6 +3,7 @@
 // 3) Paste your Project URL and anon/public key below.
 // The anon key is designed to be public in browser apps. Security is enforced by RLS policies.
 window.SALES_APP_CONFIG = {
+  adminEmails: ['jonathan.gaming1112@gmail.com'],
   supabaseUrl: "https://aoxjonpwobucxgzvbbin.supabase.co",
   supabaseAnonKey: "sb_publishable_HdmmJIKQvZQeSU9OVkTcSg_mw4XYwrT"
 };
